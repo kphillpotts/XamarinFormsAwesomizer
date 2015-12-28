3bee2083a4b7902064ac17e90f7c467c4864bab5
Kym Phillpotts <kphillpotts@gmail.com>
2015-12-28 19:27:26 +1000

    Added Samples

---
+++ Sample/XFAwesomizerSample/AnimationSamples.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Windows.Input;
+using XamarinFormsAwesomizer;
+
+namespace XFAwesomizerSample
+{
+    public class AnimationSamples : ContentPage
+    {
+        public ICommand tapCommand { get; private set;} 
+        private BoxView bv;
+        Picker picker;
+
+        public AnimationSamples()
+        {
+            Padding = 30;
+
+            tapCommand = new Command(PerformAnimation);
+
+            bv = new BoxView();
+            bv.HorizontalOptions = LayoutOptions.Center;
+            bv.VerticalOptions = LayoutOptions.Center;
+            bv.WidthRequest = 200;
+            bv.HeightRequest = 200;
+            bv.Color = Color.Red;
+
+            TapGestureRecognizer tap = new TapGestureRecognizer();
+            tap.Command = tapCommand;
+            bv.GestureRecognizers.Add(tap);
+
+            picker = new Picker();
+            picker.Items.Add("Shake Horizontal");
+            picker.Items.Add("Shake Vertical");
+            picker.SelectedIndex = 0;
+
+            var layout = new StackLayout();
+            layout.Children.Add(bv);
+            layout.Children.Add(picker);
+            this.Content = layout;
+        }
+
+        public void PerformAnimation()
+        {
+            switch (picker.Items[picker.SelectedIndex])
+            {
+                case "Shake Horizontal":
+                    bv.ShakeHorizontally();
+                    break;
+                case "Shake Vertical":
+                    bv.ShakeVertically();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+
---
+++ Sample/XFAwesomizerSample/XFAwesomizerSample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xamarin.Forms;
+using System.Linq.Expressions;
+
+namespace XFAwesomizerSample
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new AnimationSamples();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
+
